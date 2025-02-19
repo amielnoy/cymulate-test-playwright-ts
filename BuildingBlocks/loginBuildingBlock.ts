91b3026735c3918574064629adc04289ac37438c
Netanel Harush <netanelharush2@gmail.com>
2025-02-19 11:50:58 +0200

    refactor: improve code formatting and structure in loginBuildingBlock, testSetup, and ReportsPage

--- BuildingBlocks/loginBuildingBlock.ts
+++ BuildingBlocks/loginBuildingBlock.ts
@@ -1,38 +1,32 @@
-import {Page} from "@playwright/test";
-import {LoginPage} from "../Pages/loginPage";
+import {Page} from '@playwright/test'
+import {LoginPage} from '../Pages/loginPage'
 // @ts-ignore
-import dotenv from 'dotenv';
+import dotenv from 'dotenv'
 
 // Load environment variables from .env file
-
+dotenv.config()
 
 export class LoginBuildingBlock {
+  readonly page: Page
+  readonly loginPage: LoginPage
 
-    readonly page: Page;
-    readonly loginPage: LoginPage;
+  constructor(page: Page) {
+    this.page = page
+    this.loginPage = new LoginPage(page)
+  }
 
-    constructor(page: Page) {
-        this.page = page;
-        this.loginPage = new LoginPage(page);
+  async signIn(): Promise<void> {
+    const email = process.env.EMAIL
+    const password = process.env.PASSWORD
 
+    await this.loginPage.gotoLoginPage()
+
+    if (email && password) {
+      await this.loginPage.setEmail(email)
+      await this.loginPage.setPassword(password)
+      await this.loginPage.clickSignInButton()
+    } else {
+      throw new Error('Email or Password environment variable is not set.')
     }
-
-
-    async signIn(): Promise<void> {
-        const email = process.env.EMAIL;
-        const password = process.env.PASSWORD;
-
-        await this.loginPage.gotoLoginPage();
-
-        if (email && password) {
-            await this.loginPage.setEmail(email);
-            await this.loginPage.setPassword(password);
-            await this.loginPage.clickSignInButton();
-        } else {
-            throw new Error("Email or Password environment variable is not set.");
-        }
-    }
-
-
-
+  }
 }
